9ca0ce581ac53ae3f2ff37a6a82669e065170d3b
Pravarakya Kayithoju <pravarakyakayithoju2007@gmail.com>
2026-02-11 12:06:36 +0530

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl text-green-500 font-bold">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  );
+}
